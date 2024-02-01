3a2aac474ee552f383e5ef81300cad972e19f8ad
chuchoramirez195@gmail.com <chuchoramirez195@gmail.com>
2024-01-31 21:21:37 -0600

    Saving the notes in the component state

--- FONotes/src/App.jsx
+++ FONotes/src/App.jsx
@@ -1,6 +1,15 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -9,6 +18,11 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+
+      <form onSubmit={addNote}>
+        <input />
+        <button type="submit">save</button>
+      </form>   
     </div>
   )
 }
